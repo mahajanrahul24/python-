55510ef08fb04fdc38d5b6c4f1cd41bdc224aa34
Rahul Mahajan <36437143+mahajanrahul24@users.noreply.github.com>
2018-11-19 15:17:08 +0100

    Update Task2.py

--- Task_2/Task2.py
+++ Task_2/Task2.py
@@ -26,7 +26,7 @@ def peak_counter(column, threshold):
             Number of peaks in given range
     '''
     #filtered_data = DATA_FIELD[column].iloc[lower_bound:upper_bound]
-    filtered_data = (DATA_FIELD[column] - DATA_FIELD[column].mean())#/DATA_FIELD[column].std()
+    filtered_data = (DATA_FIELD[column] - DATA_FIELD[column].mean())
     peak, _ = find_peaks(filtered_data, height=threshold)
     num_peak = len(peak)
     return num_peak
